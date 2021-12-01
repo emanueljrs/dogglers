7a3aab9a7c6f0effa16cfe5312ac04b4a16f3133
Emanuel Rodrigues <emanuelrootsneite@gmail.com>
2021-12-01 06:49:20 -0300

    implemented home screen

--- app/src/main/java/com/emanuel/dogglers/MainActivity.kt
+++ app/src/main/java/com/emanuel/dogglers/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.emanuel.dogglers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emanuel.dogglers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var layoutType: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setListeners()
     }
+
+    private fun setListeners() {
+        binding.buttonVertical.setOnClickListener {
+            layoutType = "vertical"
+   //         startActivityList(layoutType)
+        }
+
+        binding.buttonHorizontal.setOnClickListener {
+            layoutType = "horizontal"
+ //           startActivityList(layoutType)
+        }
+
+        binding.buttonGrid.setOnClickListener {
+            layoutType = "grid"
+ //           startActivityList(layoutType)
+        }
+    }
+
 }
